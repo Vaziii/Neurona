dc3250d681d125fbaf6d57a5b609dc4f1dddebfb
Vaziii <elkinibarra81@gmail.com>
2026-02-02 09:38:24 -0500

    Correcion de paths

--- src/main/java/utilities/Paths.java
+++ src/main/java/utilities/Paths.java
@@ -6,9 +6,7 @@ public class Paths {
     public static final String MainMenu = "/MainMenu.fxml";
     public static final String IngresoView = "/IngresoView.fxml";
     public static final String OlvidoView = "/OlvidoView.fxml";
-    public static final String VisualizadorView = "/VisualizadorArbol" +
-            ".fxml";
-
+    public static final String VisualizadorView = "/VisualizadorArbol.fxml";
     public static final String CSS = "/estilos.css";
 
 }
